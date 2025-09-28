3a780a2d16f756ef9139d611fe26e3b8759135e7
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-09-28 08:59:25 +0700

    memulai belajar C Sharp

---
+++ udemy/LearnCSharpCodingBasicsforBeginnersCSharpFundamentals/HelloWorldProgram/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace HelloWorldProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Yan";
+            string mobile = "1234567890";
+
+            Console.WriteLine(name);
+            Console.WriteLine(mobile);
+
+            int myNum = 10;
+            Console.WriteLine(myNum);
+
+            double myDouble = 5.99D;
+            char myLetter = 'D';
+            bool myBool = true;
+
+            string firstName = "John";
+            string lastName = "Doe";
+            string fullName = firstName + " " + lastName;
+
+            Console.WriteLine("Hello " + fullName);
+
+            int a = 10;
+            int b = 20;
+            int c = a + b;
+
+            Console.WriteLine(c);
+        }
+    }
+}
