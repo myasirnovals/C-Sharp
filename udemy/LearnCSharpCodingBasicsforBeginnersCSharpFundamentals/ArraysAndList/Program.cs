b06f69eb43dc12eb69bd8b800767b87d08b51c98
Muhamad Yasir Noval <myasirnoval@gmail.com>
2025-10-08 08:58:15 +0700

    update today

---
+++ udemy/LearnCSharpCodingBasicsforBeginnersCSharpFundamentals/ArraysAndList/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ArraysAndList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Arrays
+            string[] travels = { "Car", "Bus", "Plane" };
+            int[] myNumbers = { 1, 2, 3, 4, 5 };
+
+            travels[0] = "Train";
+
+            Console.WriteLine(travels[0]);
+            Console.WriteLine(myNumbers[0]);
+            Console.WriteLine(travels.Length);
+
+            // Loop through an array
+            foreach (var travel in travels)
+            {
+                Console.WriteLine(travel);
+            }
+
+            // List
+            var cities = new List<string>();
+
+            // add a list
+            cities.Add("Jakarta");
+            cities.Add("Bandung");
+            cities.Add("Surabaya");
+
+            var country = new List<string>
+            {
+                "Indonesia", "Malaysia", "Singapore"
+            };
+        }
+    }
+}
--- udemy/LearnCSharpCodingBasicsforBeginnersCSharpFundamentals/ControlFlowStatements/Program.cs
+++ udemy/LearnCSharpCodingBasicsforBeginnersCSharpFundamentals/ControlFlowStatements/Program.cs
@@ -13,7 +13,7 @@ namespace myFirstProject
                 Console.WriteLine("While Loop: " + i);
                 i++;
             }
-            
+
             // Do While Loop
             i = 100;
             do
@@ -21,11 +21,43 @@ namespace myFirstProject
                 Console.WriteLine("Do While Loop: " + i);
                 i++;
             } while (i < 10);
-            
+
             // For Loop
             for (int j = 0; j < 5; j++)
             {
                 Console.WriteLine("For Loop: " + j);
+            }
+
+            // For Each statement
+            string[] travels = { "Car", "Bus", "Train", "Plane" };
+            foreach (string travel in travels)
+            {
+                Console.WriteLine("For Each Loop: " + travel);
+            }
+
+            // break and continue
+            for (int j = 0; j < 10; j++)
+            {
+                if (j % 2 == 0)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("For Loop: " + j);
+                if (j == 5)
+                {
+                    break;
+                }
+            }
+            
+            // triangle star
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < row; col++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
             }
         }
     }
